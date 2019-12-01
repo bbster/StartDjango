88950c8df0c8f3382c369025e303d7c7f79fafb8
bbster <bbster12@navr.com>
2019-12-01 20:30:01 +0900

    start

---
+++ myapp/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+
+class First():
+
+    def create(self, request):
+        pass
+
+    def retrieve(self, request):
+        pass
+
+    def update(self, request):
+        pass
+
+    def delete(self, request):
+        pass
+
+
